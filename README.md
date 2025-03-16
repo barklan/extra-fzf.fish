3c0a480e425b002dcd966308eeb656d55e295673
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-03-16 12:59:40 +0300

    fixup!

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # extra-fzf.fish
 
-Gutted fork of [jethrokuan/fzf](https://github.com/jethrokuan/fzf) with some extras to work with [PatrickF1/fzf.fish](https://github.com/PatrickF1/fzf.fish).
+Gutted fork of [jethrokuan/fzf](https://github.com/jethrokuan/fzf).
 
 ## Usage
 
 Call `fzf_configure_dunder_bindings` somewhere in `config.fish`.
 Default keymaps are:
 
-- `Alt + c` - cd into sub-directories (recursively searched)
-- `Alt + Shift + c` - cd into sub-directories, including hidden ones
-- `Alt + o` - open a file/dir using xdg-open or open command
 - `Alt + g` - live ripgrep and open file
 - `Alt + Shift + g` - live ripgrep-all
--- functions/fzf_configure_dunder_bindings.fish
+++ functions/fzf_configure_dunder_bindings.fish
@@ -1,24 +1,18 @@
 function fzf_configure_dunder_bindings --description "Configure fzf functions prefixed with __"
     status is-interactive || test "$CI" = true; or return
 
-    set -q FZF_TMUX_HEIGHT; or set -U FZF_TMUX_HEIGHT "40%"
     set -q FZF_DEFAULT_OPTS; or set -U FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT"
     set -q FZF_DISABLE_KEYBINDINGS; or set -U FZF_DISABLE_KEYBINDINGS 0
     set -q FZF_PREVIEW_FILE_CMD; or set -U FZF_PREVIEW_FILE_CMD "head -n 10"
     set -q FZF_PREVIEW_DIR_CMD; or set -U FZF_PREVIEW_DIR_CMD ls
 
     if test "$FZF_DISABLE_KEYBINDINGS" -ne 1
-        # bind \ec __fzf_cd
-        bind \eC '__fzf_cd --hidden'
-        # bind \eo __fzf_open
         bind \eg __fzf_rg
         bind \eG __fzf_rga
 
         if ! test "$fish_key_bindings" = fish_default_key_bindings
-            # bind -M insert \ec __fzf_cd
-            bind -M insert \eC '__fzf_cd --hidden'
-            bind -M insert \eo __fzf_open
             bind -M insert \eg __fzf_rg
+            bind -M insert \eG __fzf_rga
         end
 
         if not bind --user \t >/dev/null 2>/dev/null
